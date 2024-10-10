7cbd4f0fc28fc5f42f2f81d8630aff2f36827599
ssnyta <94050114+ssnyta@users.noreply.github.com>
2024-10-10 09:09:59 +0200

    .

---
+++ factory/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace factory
+{
+
+    class Program {
+        static void Main (string[]args){
+            Auto Fabia = new Auto ("Škoda", "Fabia");
+            var Felda = new TovarnaNaAuta();
+            Console.WriteLine(Fabia.Vrrrrrrr());
+        }
+    }
+    class Auto
+    {
+        private string znacka;
+        private string model;
+        public Auto(string znacka, string model)
+        {
+            this.znacka = znacka;
+            this.model = model;
+        }
+        public string Vrrrrrrr(){
+            return $"{znacka}, {model}";
+        }
+    }
+
+    class TovarnaNaAuta
+    {
+        public Auto VytvorFelicii()
+        {
+            return new Auto("Škoda", "Felicia");
+        }
+    }
+}
+
